bfb2ed599fa1bba772bb39faf5876d7907a55603
raj kushwaha <rajkushwah9968117019@gmail.com>
2022-08-18 19:51:16 +0530

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { MantineProvider } from "@mantine/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <MantineProvider
+      theme={{
+        fontFamily: "Inter, sans-serif",
+        headings: { fontFamily: "Montserrat, sans-serif" },
+      }}
+      withGlobalStyles
+      withNormalizeCSS
+    >
+      <Component {...pageProps} />
+    </MantineProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
