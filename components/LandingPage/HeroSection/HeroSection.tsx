ebe3d79bd1ad4740d3cd7436af5440d863035a69
raj kushwaha <rajkushwah9968117019@gmail.com>
2022-08-18 20:11:59 +0530

    initial commit on dev branch

---
+++ components/LandingPage/HeroSection/HeroSection.tsx
@@ -0,0 +1,7 @@
+import styles from "./HeroSection.module.scss";
+
+const HeroSection = () => {
+  return <header className={styles.header}>this is the hero section</header>;
+};
+
+export default HeroSection;
