e4da4b774dcf90d8f44b9cb546f5e6f41901dbcf
raj kushwaha <rajkushwah9968117019@gmail.com>
2022-09-05 17:39:42 +0530

    finished footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -4,23 +4,29 @@ import logo from "../../assets/images/logo.png";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <ul className={styles["footer-links"]}>
-        <li className={styles["footer-link"]}>About</li>
-        <li className={styles["footer-link"]}>Terms of Use</li>
-        <li className={styles["footer-link"]}>Privacy Policy</li>
-        <li className={styles["footer-link"]}>Careers</li>
-        <li className={styles["footer-link"]}>Blog</li>
-        <li className={styles["footer-link"]}>Contact</li>
-        <li className={styles["footer-link"]}>
-          <a
-            target="_blank"
-            rel="noreferrer"
-            href="https://github.com/Raj-divine/meethub"
-          >
-            <strong>Github</strong>
-          </a>
-        </li>
-      </ul>
+      <div className={styles["footer-links-container"]}>
+        <ul className={styles["footer-links"]}>
+          <li className={styles["footer-link"]}>About</li>
+          <li className={styles["footer-link"]}>Terms of Use</li>
+          <li className={styles["footer-link"]}>Privacy Policy</li>
+        </ul>
+
+        <ul className={styles["footer-links"]}>
+          <li className={styles["footer-link"]}>Careers</li>
+          <li className={styles["footer-link"]}>Blog</li>
+          <li className={styles["footer-link"]}>Contact</li>
+          <li className={styles["footer-link"]}>
+            <a
+              target="_blank"
+              rel="noreferrer"
+              href="https://github.com/Raj-divine/meethub"
+            >
+              <strong>Github</strong>
+            </a>
+          </li>
+        </ul>
+      </div>
+
       <div className={styles["image-container"]}>
         <Image src={logo} alt="logo" layout="intrinsic" objectFit="contain" />
       </div>
