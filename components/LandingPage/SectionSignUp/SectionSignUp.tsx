79a25d871f681a4c41487b18a2aebc2f5d1cd6af
raj kushwaha <rajkushwah9968117019@gmail.com>
2022-09-05 17:43:08 +0530

    added scroll animation in signup section

--- components/LandingPage/SectionSignUp/SectionSignUp.tsx
+++ components/LandingPage/SectionSignUp/SectionSignUp.tsx
@@ -1,7 +1,16 @@
 import styles from "./SectionSignUp.module.scss";
+import { useScrollAnimation } from "../../../hooks";
 const SectionSignUp = () => {
+  const { ref: sectionRef, isIntersecting } = useScrollAnimation({
+    threshold: 0.2,
+  });
   return (
-    <section className={styles["section-sign-up"]}>
+    <section
+      ref={sectionRef}
+      className={`${styles["section-sign-up"]} ${
+        isIntersecting ? styles.animate : ""
+      }`}
+    >
       <p>Do not waste time thinking</p>
       <p>Create your meethub account today</p>
       <button>Join now!</button>
