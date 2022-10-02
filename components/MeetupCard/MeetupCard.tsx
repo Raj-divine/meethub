445a7e9ca3e845f51c528cdbfb0eaa6a5741c73a
raj kushwaha <rajkushwah9968117019@gmail.com>
2022-10-02 19:07:21 +0530

    added category in meetup-card

--- components/MeetupCard/MeetupCard.tsx
+++ components/MeetupCard/MeetupCard.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import styles from "./MeetupCard.module.scss";
 import dayjs from "dayjs";
-import { Text } from "@mantine/core";
+import { Text, Tooltip } from "@mantine/core";
 import { DocumentData } from "firebase/firestore";
 
 const MeetupCard = ({ meetup }: DocumentData) => {
-  const { title, description, date, price, image } = meetup;
+  const { title, description, date, price, image, category } = meetup;
   return (
     <div className={styles["meetup-card"]}>
       <div className={styles["card-image"]}>
@@ -21,6 +21,11 @@ const MeetupCard = ({ meetup }: DocumentData) => {
         <div className={styles["card-info"]}>
           <div className={styles.date}>{dayjs(date).format("DD MMM")}</div>
           <div className={styles.price}>{price < 1 ? "Free" : `₹${price}`}</div>
+          <Tooltip label={category}>
+            <div className={styles.category}>
+              <Text lineClamp={1}>{category}</Text>
+            </div>
+          </Tooltip>
         </div>
       </div>
     </div>
