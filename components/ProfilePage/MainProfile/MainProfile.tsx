a9461df3307c608daef7acaf22542c5049d93bdd
raj kushwaha <rajkushwah9968117019@gmail.com>
2022-11-01 16:41:46 +0530

    added main profile section

--- components/ProfilePage/MainProfile/MainProfile.tsx
+++ components/ProfilePage/MainProfile/MainProfile.tsx
@@ -1,8 +1,22 @@
+import { Text } from "@mantine/core";
 import { DocumentData } from "firebase/firestore";
+import Image from "next/image";
 import styles from "./MainProfile.module.scss";
 
 const MainProfile = ({ user }: DocumentData) => {
-  return <div className={styles["main-profile"]}>this is the main profile</div>;
+  return (
+    <div className={styles["main-profile"]}>
+      <div className={styles["profile-picture"]}>
+        <Image src={user.profilePicture} layout="fill" objectFit="cover" />
+      </div>
+      <div className={styles.info}>
+        <Text className={styles["full-name"]}>{user.fullName}</Text>
+        <Text color="dimmed" className={styles.email}>
+          {user.email}
+        </Text>
+      </div>
+    </div>
+  );
 };
 
 export default MainProfile;
